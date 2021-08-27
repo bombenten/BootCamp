a8c1ba8760e1eda8d8c6ffe5d0d1049d6efb3b87
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-08-27 15:05:17 +0700

    seconday
    -ชนกันได้
    -ชนแล้วหาย

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,11 @@
 import Phaser from "phaser";
 let player;
 let backGround;
+let monster;
+let event;
+let objmonster;
+let monsterDestroy;
+
 //Controller
 let keyW;
 let keyA;
@@ -18,8 +23,10 @@ class GameScene extends Phaser.Scene {
          // BackGround
        this.load.image('TileSprite', 'src/image/background_Jerom.png');
        // Character
-       this.load.spritesheet('player','/src/image/Pacman.png',
+       this.load.spritesheet('player','/src/image/Bababoi.png',
        {frameWidth: 750 , frameHeight: 828});
+       this.load.spritesheet('monster','src/image/monster.png',
+       {frameWidth: 17.3 , frameHeight: 18});
       
     
     }
@@ -29,13 +36,20 @@ class GameScene extends Phaser.Scene {
          backGround = this.add.tileSprite(0,0,850,1400, 'TileSprite').setOrigin(0,0).setScale(8);
 
          
-         player = this.physics.add.sprite(150,300,'player')
-    .setScale(0.4)
+         player = this.physics.add.sprite(220,700,'player')
+    .setScale(0.2)
     .setImmovable()
-   .setCollideWorldBounds(true); 
-
-
-   // Monster
+   .setCollideWorldBounds(true);
+   
+//    monster= this.physics.add.sprite(150,100,'monster') 
+//    .setScale(1)
+//    .setImmovable()
+//   .setCollideWorldBounds(true);
+  //oject 
+       // monster = this.physics.add.Group(monster);
+     
+    
+   // player
    this.anims.create({
     key: 'playerAni',
     frames: this.anims.generateFrameNumbers('player', {
@@ -45,6 +59,41 @@ class GameScene extends Phaser.Scene {
     duration: 400,    
     repeat: -1
 });
+
+//monster
+this.anims.create({
+    key: 'monsterAni',
+    frames: this.anims.generateFrameNumbers('monster', {
+        start: 0,
+        end: 7
+    }),
+    duration: 1500,    
+    repeat: -1
+});
+ 
+objmonster = this.physics.add.group();
+    
+event = this.time.addEvent({
+    delay: 1000,
+    callback: function () {
+        monster = this.physics.add.sprite(Phaser.Math.Between(0, 450) , -50, 'monster')
+        .setScale(3)
+        .setImmovable()
+   
+        monster.anims.play('monsterAni', true);
+        objmonster.add(monster);
+        monster.setVelocityY(Phaser.Math.Between(500, 700));
+        this.physics.add.overlap(monster, player, monsterDestroy);
+    },
+    callbackScope: this,
+    loop: true,
+    paused: false,
+});
+
+function monsterDestroy(monster, player) {
+    monster.destroy();
+
+} 
 
 keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -58,7 +107,11 @@ keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
        backGround.tilePositionY -= 0.1;
        //animetion player
        player.anims.play('playerAni',true)
+       //animetion monster
+       //monster.anims.play('monsterAni',true)
 
+       
+      
         // Player Control
         if(keyW.isDown){
             player.setVelocityY(-500);
@@ -73,6 +126,12 @@ keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
             player.setVelocityX(500);
         }else{
             player.setVelocityX(0);
+        }
+         // Monster Destroy
+         for (let i = 0; i < objmonster.getChildren().length; i++) {
+            if (objmonster.getChildren()[i].y > 700) {
+                objmonster.getChildren()[i].destroy();
+            }
         }
     }
 }
