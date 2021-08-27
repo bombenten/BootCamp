013f1e43990cf25d45f0c8110669c778e203d6ed
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-08-27 19:09:53 +0700

    final
    ตามโจทย์อย่างสมบูร

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,9 +2,13 @@ import Phaser from "phaser";
 let player;
 let backGround;
 let monster;
-let event;
+let eventmonster;
 let objmonster;
-let monsterDestroy;
+let objem;
+let keyFire ;
+let emSpeed =900 ;
+let nakom;
+let Bababoi;
 
 //Controller
 let keyW;
@@ -23,11 +27,13 @@ class GameScene extends Phaser.Scene {
          // BackGround
        this.load.image('TileSprite', 'src/image/background_Jerom.png');
        // Character
-       this.load.spritesheet('player','/src/image/Bababoi.png',
-       {frameWidth: 750 , frameHeight: 828});
+       this.load.spritesheet('player','/src/image/Bababoi1.png',
+       {frameWidth: 224 , frameHeight: 225});
        this.load.spritesheet('monster','src/image/monster.png',
        {frameWidth: 17.3 , frameHeight: 18});
-      
+       this.load.image('em','src/image/submarine .png');
+       this.load.audio('nakom','src/sound/nakom.mp3')
+       this.load.audio('Ba','src/sound/bababoi-sound-effect.mp3')
     
     }
 
@@ -35,12 +41,18 @@ class GameScene extends Phaser.Scene {
          // Background
          backGround = this.add.tileSprite(0,0,850,1400, 'TileSprite').setOrigin(0,0).setScale(8);
 
-         
-         player = this.physics.add.sprite(220,700,'player')
-    .setScale(0.2)
+         //player
+         player = this.physics.add.sprite(190,600,'player')
+    .setScale(0.7)
     .setImmovable()
-   .setCollideWorldBounds(true);
+   .setCollideWorldBounds(true)
+   .setSize(120,225)
+   .setOffset(40,0)
+   ;
    
+        //sound
+        nakom =this.sound.add('nakom');
+        Bababoi = this.sound.add('Ba');
 //    monster= this.physics.add.sprite(150,100,'monster') 
 //    .setScale(1)
 //    .setImmovable()
@@ -49,7 +61,7 @@ class GameScene extends Phaser.Scene {
        // monster = this.physics.add.Group(monster);
      
     
-   // player
+   // player animetion
    this.anims.create({
     key: 'playerAni',
     frames: this.anims.generateFrameNumbers('player', {
@@ -60,7 +72,7 @@ class GameScene extends Phaser.Scene {
     repeat: -1
 });
 
-//monster
+//monster animetion
 this.anims.create({
     key: 'monsterAni',
     frames: this.anims.generateFrameNumbers('monster', {
@@ -70,11 +82,16 @@ this.anims.create({
     duration: 1500,    
     repeat: -1
 });
- 
+
+ //group
 objmonster = this.physics.add.group();
-    
-event = this.time.addEvent({
-    delay: 1000,
+objem = this.physics.add.group();
+
+
+
+//event
+eventmonster = this.time.addEvent({
+    delay: 300,
     callback: function () {
         monster = this.physics.add.sprite(Phaser.Math.Between(0, 450) , -50, 'monster')
         .setScale(3)
@@ -83,32 +100,47 @@ event = this.time.addEvent({
         monster.anims.play('monsterAni', true);
         objmonster.add(monster);
         monster.setVelocityY(Phaser.Math.Between(500, 700));
-        this.physics.add.overlap(monster, player, monsterDestroy);
+        
+        this.physics.add.overlap(monster, player, function monsterDestroy(monster, player) {
+            nakom.play();
+            monster.destroy();
+            console.log(true);
+        
+        } );
     },
     callbackScope: this,
     loop: true,
     paused: false,
 });
 
-function monsterDestroy(monster, player) {
+
+
+
+    //Emoji hit
+this.physics.add.overlap(objmonster,objem,function onHit(monster, em){
+    Bababoi.play();
+    em.destroy()
     monster.destroy();
-
-} 
-
+    console.log(false);
+});
+    
+//controll
 keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
 keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-
-    }
+keyFire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+}
+    
     
     update(delta, time) {
        // Background
        backGround.tilePositionY -= 0.1;
-       //animetion player
-       player.anims.play('playerAni',true)
+       
+       
        //animetion monster
-       //monster.anims.play('monsterAni',true)
+      
+       player.anims.play('playerAni',true);
 
        
       
@@ -127,13 +159,31 @@ keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         }else{
             player.setVelocityX(0);
         }
-         // Monster Destroy
+         //Monster Destroy
          for (let i = 0; i < objmonster.getChildren().length; i++) {
             if (objmonster.getChildren()[i].y > 700) {
                 objmonster.getChildren()[i].destroy();
             }
         }
+             //fire and update
+        if (Phaser.Input.Keyboard.JustDown(keyFire)) {
+            let em = this.physics.add.sprite(player.x, player.y, "em");
+            em.setScale(0.2);
+            em.setSize(200,512);
+            em.setOffset(200,0);
+            objem.add(em);
+            em.body.velocity.y = -emSpeed;
+            
+            
+        }
+        for (let i = 0; i < objem.getChildren().length; i++) {
+            let em = objem.getChildren()[i];
+            if (em.y < -40) {
+                em.destroy();
+            }
+        }
+        
     }
-}
 
+}
 export default GameScene;
