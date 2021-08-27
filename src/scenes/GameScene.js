1bd32a8426d3f5cc54b7ed5e72eae4d7a84a7b73
AthitiJames <69097916+AthitiJames@users.noreply.github.com>
2021-08-27 21:30:20 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,6 @@
 import Phaser from "phaser";
 
 let backGround;
-let cookiePlayer;
 let monster;
 let goldManPlayer;
 let bullet;
@@ -17,7 +16,6 @@ let keyFire;
 let bulletspeed = -200;
 
 //event
-let bulletEvent;
 let monsterEvent;
 let monsterGroup;
 let bulletGroup;
@@ -32,31 +30,39 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('map', 'src/img/map.jpg');
 
-        this.load.spritesheet('goldMan', 'src/img/goldman.png',
-        {frameWidth: 39.33333333333333, frameHeight: 41});
+        this.load.spritesheet('goldMan', 'src/img/goldman.png', {
+            frameWidth: 39.33333333333333,
+            frameHeight: 41
+        });
 
-        this.load.spritesheet('monster', 'src/img/monster2.png',
-        {frameWidth: 52, frameHeight: 64});
+        this.load.spritesheet('monster', 'src/img/monster2.png', {
+            frameWidth: 52,
+            frameHeight: 64
+        });
 
-        this.load.spritesheet('bulletcoin', 'src/img/spinning-coin-spritesheet-removebg-preview.png',
-        {frameWidth: 500, frameHeight: 35.71428571428571});
+        this.load.spritesheet('bulletcoin', 'src/img/spinning-coin-spritesheet-removebg-preview.png', {
+            frameWidth: 500,
+            frameHeight: 35.71428571428571
+        });
 
     }
 
     create() {
         //Show X Y
-        this.label = this.add.text(0, 0, '(x, y)', { fontFamily: '"Monospace"' })
+        this.label = this.add.text(0, 0, '(x, y)', {
+                fontFamily: '"Monospace"'
+            })
             .setDepth(100);
         this.pointer = this.input.activePointer;
 
         backGround = this.add.tileSprite(0, 0, 450, 720, 'map')
-        .setOrigin(0, 0)
-        .setScale(1.17);
+            .setOrigin(0, 0)
+            .setScale(1.17);
 
         goldManPlayer = this.physics.add.sprite(235, 630, 'goldMan')
-        .setImmovable()
-        .setCollideWorldBounds(true)
-        .setScale(2);
+            .setImmovable()
+            .setCollideWorldBounds(true)
+            .setScale(2);
 
         monsterGroup = this.physics.add.group();
 
@@ -65,52 +71,50 @@ class GameScene extends Phaser.Scene {
             delay: Phaser.Math.Between(500, 1000),
             callback: function () {
                 monster = this.physics.add.sprite(Phaser.Math.Between(1, 450), 0, 'monster')
-                .setScale(2);
+                    .setScale(2);
                 monsterGroup.add(monster);
                 monsterGroup.setVelocityY(200);
-                
-                monster.anims.play('animonster',true);
+
+                monster.anims.play('animonster', true);
                 this.physics.add.overlap(bulletGroup, monster, DestroyMonster);
             },
             callbackScope: this,
             loop: true,
             paused: false
         })
-        
+
         bulletGroup = this.physics.add.group();
-        
-        
+
+
 
         this.anims.create({
             key: 'anigoldman',
-            frames: this.anims.generateFrameNumbers('goldMan',{
-                start:0,
-                end:5
+            frames: this.anims.generateFrameNumbers('goldMan', {
+                start: 0,
+                end: 5
             }),
-            duration:900,
+            duration: 900,
             repeat: -1
         });
 
         this.anims.create({
             key: 'animonster',
-            frames: this.anims.generateFrameNumbers('monster',{
-                start:0,
-                end:7
+            frames: this.anims.generateFrameNumbers('monster', {
+                start: 0,
+                end: 7
             }),
-            duration:900,
+            duration: 900,
             repeat: -1
         });
 
-        
 
-        
 
-        function DestroyMonster(bullet, monster){
+        function DestroyMonster(bullet, monster) {
             monster.destroy();
             bullet.destroy();
 
         }
-        
+
         //Player Pointermove
         // this.input.on('pointermove', (pointer)=>{
         //     cookiePlayer.x = pointer.x
@@ -124,68 +128,68 @@ class GameScene extends Phaser.Scene {
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         keyFire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        
+
     }
-    
+
     update(delta, time) {
         //Show X Y
         this.label.setText('(' + this.pointer.x + ', ' + this.pointer.y + ')');
-        
+
         backGround.tilePositionY -= 1.5;
 
-        
-        goldManPlayer.anims.play('anigoldman',true);
-        
-        
+
+        goldManPlayer.anims.play('anigoldman', true);
+
+
 
         //setRun
-        if(keyW.isDown){
+        if (keyW.isDown) {
             goldManPlayer.setVelocityY(-500);
-        }else if(keyS.isDown){
+        } else if (keyS.isDown) {
             goldManPlayer.setVelocityY(500);
-        }else{
+        } else {
             goldManPlayer.setVelocityY(0);
         }
-        if(keyA.isDown){
+        if (keyA.isDown) {
             goldManPlayer.setVelocityX(-500);
-        }else if(keyD.isDown){
+        } else if (keyD.isDown) {
             goldManPlayer.setVelocityX(500);
-        }else{
+        } else {
             goldManPlayer.setVelocityX(0);
         }
 
-        if(Phaser.Input.Keyboard.JustDown(keyFire)){
-            bullet = this.physics.add.sprite(goldManPlayer.x,goldManPlayer.y,"bulletcoin")
-            .setImmovable()
-            .setScale(1)
-            .setSize(1);
+        if (Phaser.Input.Keyboard.JustDown(keyFire)) {
+            bullet = this.physics.add.sprite(goldManPlayer.x, goldManPlayer.y, "bulletcoin")
+                .setImmovable()
+                .setScale(1)
+                .setSize(1);
             // bulletGroup.body.velocity.y= -bulletspeed;
             bulletGroup.add(bullet);
             bulletGroup.setVelocityY(-200);
 
             this.anims.create({
                 key: 'anicoin',
-                frames: this.anims.generateFrameNumbers('bulletcoin',{
-                    start:0,
-                    end:13
+                frames: this.anims.generateFrameNumbers('bulletcoin', {
+                    start: 0,
+                    end: 13
                 }),
-                duration:900,
+                duration: 900,
                 repeat: -1
             });
-            bullet.anims.play('anicoin',true);
-            
+            bullet.anims.play('anicoin', true);
+
         }
-        
+
         for (let i = 0; i < monsterGroup.getChildren().length; i++) {
             if (monsterGroup.getChildren()[i].y > 700) {
                 monsterGroup.getChildren()[i].destroy();
             }
         }
-        
+
 
         for (let i = 0; i < bulletGroup.getChildren().length; i++) {
             if (bulletGroup.getChildren()[i].y > 700) {
-                    bulletGroup.getChildren()[i].destroy();
+                bulletGroup.getChildren()[i].destroy();
             }
         }
     }
